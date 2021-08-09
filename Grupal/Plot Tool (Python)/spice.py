56c0fe6f7a9c81b5f2e1254cac3c0e3eef869cb7
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2021-08-09 13:59:17 -0300

    Nuevo archivo curva

--- Grupal/Plot Tool (Python)/spice.py
+++ Grupal/Plot Tool (Python)/spice.py
@@ -150,17 +150,17 @@ def calcRta(H, exitacion, w=0, A=0, duty=0.5):
     if exitacion == 0:                      # escalon
         t, y = ss.step((H.num, H.den))      #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.step.html#scipy.signal.step
         
-    elif exitacion == 1:            # senoidal
+    elif exitacion == 1:                    # senoidal
         tMax= 5 * (2*np.pi/w)   
         tin = np.linspace(0, tMax, 50000, endpoint=False)
         seno = A*(np.sin(w*t))
         
         t, y,  = ss.lsim((H.num, H.den), U=seno, T=t)  # Calculamos la Rta
     
-    elif exitacion == 2:            # impulso
-        t, y = ss.impulse((H.num, H.den))  #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.impulse.html#scipy.signal.impulse
+    elif exitacion == 2:                    # impulso
+        t, y = ss.impulse((H.num, H.den))   #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.impulse.html#scipy.signal.impulse
 
-    elif exitacion == 3:            # pulso
+    elif exitacion == 3:                    # pulso
         tMax= 5 * (2*np.pi/w)   
         tin = np.linspace(0, tMax, 50000, endpoint=False)
         cuadrada = ss.square(5*(2*np.pi)*tin, duty)
@@ -169,62 +169,7 @@ def calcRta(H, exitacion, w=0, A=0, duty=0.5):
     #else:
     # Cargar archivo de exitación. 
 
-    return  t, y
-
-
-
-class Curve:
-
-   def __init__(self, nombre, trazo, Hnum=0, Hden=0, path, modo):
-   
-        self.nombre = nombre
-        self.path = path
-
-        if Hnum != 0 and Hden != 0:
-            self.H = getTransfFunct(Hnum, Hden)
-
-        if ".txt" in path:
-            self.data = getDataSimulation(path, modo)
-            self.trazo = "solid"
-
-        elif ".csv" in path:
-            self.data = getDataMediciones(path, modo)
-            self.trazo= "dashdot"
-
-        elif Hnum != 0 and Hden != 0:
-            self.data = getDataTeorica(self.H)
-            self.trazo = "dotted"
-
-
-    def calcRta(self, exitacion, w=0, A=0, duty=0.5):
-    
-        if exitacion == 0:                      # escalon
-            t, y = ss.step((self.H.num, self.H.den))      #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.step.html#scipy.signal.step
-            
-        elif exitacion == 1:            # senoidal
-            tMax= 5 * (2*np.pi/w)   
-            tin = np.linspace(0, tMax, 50000, endpoint=False)
-            seno = A*(np.sin(w*t))
-            
-            t, y,  = ss.lsim((self.H.num, self.H.den), U=seno, T=t)  # Calculamos la Rta
-        
-        elif exitacion == 2:            # impulso
-            t, y = ss.impulse((self.H.num, self.H.den))  #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.impulse.html#scipy.signal.impulse
-
-        elif exitacion == 3:            # pulso
-            tMax= 5 * (2*np.pi/w)   
-            tin = np.linspace(0, tMax, 50000, endpoint=False)
-            cuadrada = ss.square(5*(2*np.pi)*tin, duty)
-            t, y,  = ss.lsim((self.H.num, self.H.den), U=cuadrada, T=t)  # Calculamos la Rta
-
-        #else:
-        # Cargar archivo de exitación. 
-
-        self.data["time"] = t   # Guardamos el arreglo temporal
-        self.data["y"] = y      # Guardamos el arreglo Y
-
-    def graphCurve(self, axes):
-
+    return  t, y        
 
 
 #Tests
