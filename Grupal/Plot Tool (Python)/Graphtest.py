7946a8aec989ef2010c7ab904e2d7cf7a8c968a2
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2021-08-10 15:11:42 -0300

    Me había olvidado de activar la exitación

--- Grupal/Plot Tool (Python)/Graphtest.py
+++ Grupal/Plot Tool (Python)/Graphtest.py
@@ -12,7 +12,7 @@ curves =    [Curve(nombre="Montecarlo", Hnum="", Hden="", path='inputExamples/mo
             Curve(nombre="Med", Hnum="", Hden="", path='inputExamples/Ejemplo1-medicion.csv', modo=0),
             Curve(nombre="Teórica", Hnum="1,0,1", Hden="1,1,1", path='', modo=0)
             ]
-exitaciones = [Excitation(name ="escalón", type=0, amp=0, freq=0, freqType='F', duty=1)]
+exitaciones = [Excitation(name ="escalón", type=0, amp=1, freq=0, freqType='F', duty=1)]
 
 graphCurves(curves=curves, axes=axis, exitaciones = exitaciones)
 
