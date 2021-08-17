c780c311e0b7bf673c276187970dd45e9753712b
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2021-08-17 00:04:45 -0300

    Mejoras en la rta. escalón

--- Grupal/Plot Tool (Python)/Curve.py
+++ Grupal/Plot Tool (Python)/Curve.py
@@ -30,11 +30,12 @@ class Curve:
     def calcRta(self, exitacion):
 
         if exitacion.type == 1:                      # escalon
-            tin,step =exitacion.getValues()
-            t, y,_ = ss.lsim((self.H.num, self.H.den), U=step, T=tin)
+            #Modo nuevo
+            #tin,step =exitacion.getValues()
+            #t, y,_ = ss.lsim((self.H.num, self.H.den), U=step, T=tin)
             #Modo viejo
-            #t, y = ss.step((self.H.num, self.H.den))        #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.step.html#scipy.signal.step
-            #y = y * exitacion.amp
+            t, y = ss.step((self.H.num, self.H.den))        #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.step.html#scipy.signal.step
+            y = y * exitacion.amp
         
         elif exitacion.type == 0:                    # senoidal
             tin,seno =exitacion.getValues()
