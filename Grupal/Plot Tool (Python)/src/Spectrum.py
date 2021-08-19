e54944cf952fe0d7c0d1653ffd10a43041012a8c
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2021-08-19 17:51:49 -0300

    Spectrum2

--- Grupal/Plot Tool (Python)/src/Spectrum.py
+++ Grupal/Plot Tool (Python)/src/Spectrum.py
@@ -7,18 +7,16 @@ n = 2*np.linspace(1, numMax, num=numMax)-1
 labels = ['$'+str(int(i))+'f_0$' for i in n] # MODO SHERMAN
 Xn = 20*np.log10(1/n)
 
-f = np.linspace(1*f0, n[len(n)-1]*f0, num=1000000)
+f = np.linspace(1*f0, n[len(n)-1]*f0, num=10000)
 R = 1e3
 C = 10e-9
 H = 20*np.log10(1/np.sqrt(np.pi**2*4*f**2*R**2*C**2 + 1))
 
+plt.xscale("log")
 plt.xticks(n*f0, labels, rotation='horizontal')
 plt.stem(n*f0, Xn, basefmt="", label='$|X_n|$  normalizado')
 plt.plot(f, H, marker="o", markersize=1.2, label='$|H(f)|$')
 plt.xlabel("$f[Hz]$")
-plt.ylabel("")
-
-plt.title("")
 plt.legend()
 plt.grid(which="both")
 plt.show()
